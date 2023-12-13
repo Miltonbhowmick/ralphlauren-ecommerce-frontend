302525d976d95804ec8db4e7147f618c45cd4731
miltonbhowmick <miltonbhowmick6@gmail.com>
2023-12-13 23:16:39 +0600

    feat: added mega product list

---
+++ components/RowMegaProduct.vue
@@ -0,0 +1,82 @@
+<template>
+	<div
+		class="mega-pack-box"
+		:style="{
+			backgroundImage: `url(${props?.backgroundImage})`,
+		}"
+	>
+		<div class="product-list">
+			<div
+				v-for="(data, idx) in props.itemList"
+				:key="'product_' + idx"
+				class="item"
+			>
+				<div class="image"><img :src="data.imageSrc" /></div>
+				<h3 class="title">{{ data.title }}</h3>
+				<ul>
+					<li v-for="(link, idx) in data.linkList" :key="idx">
+						<a>{{ link.name }}</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+// import bgGroup from "~/assets/images/bg-group.jpg";
+
+const props = defineProps({
+	backgroundImage: null,
+	itemList: null,
+});
+</script>
+
+<style scoped lang="scss">
+.mega-pack-box {
+	background-position: center;
+	background-size: cover;
+	background-repeat: no-repeat;
+	width: 100%;
+	padding: 50px 10px;
+	.product-list {
+		display: flex;
+		justify-content: center;
+
+		.item {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 15px;
+			.image {
+				width: 60%;
+			}
+			.title {
+				font-family: LeJeuneDeck-Regular, "Times New Roman", Times, serif;
+				margin: 0;
+				font-size: 40px;
+				color: #ffffff;
+				text-align: center;
+			}
+			ul {
+				margin: 0;
+				padding: 0;
+				list-style: none;
+				display: flex;
+				justify-content: center;
+				gap: 20px;
+				li {
+					a {
+						color: #ffffff;
+						text-transform: uppercase;
+						font-size: 10px;
+						text-decoration: underline;
+						text-underline-offset: 15px;
+						text-decoration-color: #ffffff;
+					}
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ pages/products/gifts.vue
@@ -0,0 +1,89 @@
+<template>
+	<CustomSlider
+		:headlineBlack="false"
+		:backgroundImage="'/images/slider-bg-blur.jpg'"
+		:isBackgroundImage="true"
+		:title="'Our Favourites'"
+		:subTitle="'Signature Ralph Lauren gifts for everyone on your list'"
+	/>
+	<RowMegaProduct :backgroundImage="bgImage" :itemList="megaProductList" />
+</template>
+
+<script setup>
+import bgImage from "~/assets/images/bg-group4.jpg";
+
+var megaProductList = [
+	{
+		title: "Gifts Under 150 €",
+		imageSrc: "/images/bag-culture.jpg",
+		linkList: [
+			{
+				name: "MEN",
+				href: "/",
+			},
+			{
+				name: "WOMEN",
+				href: "/",
+			},
+			{
+				name: "HOME",
+				href: "/",
+			},
+		],
+	},
+	{
+		title: "Gifts Under 75 €",
+		imageSrc: "/images/polo-tshirt.jpg",
+		linkList: [
+			{
+				name: "boys",
+				href: "/",
+			},
+			{
+				name: "girls",
+				href: "/",
+			},
+			{
+				name: "baby",
+				href: "/",
+			},
+		],
+	},
+];
+</script>
+
+<style scoped lang="scss">
+.kids-navbar {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	background: #041e3a;
+	ul {
+		margin: 0;
+		list-style: none;
+		display: flex;
+		gap: 30px;
+		padding: 5px 0;
+		li {
+			padding: 15px 0;
+			&.active {
+				border-bottom: 0.3125em solid white;
+			}
+			a {
+				font-family: "LeJeuneDeck-Regular", serif;
+				color: white;
+			}
+		}
+	}
+}
+.slider-bg {
+	.slider-box {
+		margin-top: -250px;
+		padding-left: 121px;
+		.title {
+			color: #fff;
+		}
+	}
+}
+</style>
