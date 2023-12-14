c24635a8a63819802c57eade653c882ebb23fbba
miltonbhowmick <miltonbhowmick6@gmail.com>
2023-12-14 22:09:37 +0600

    chore: added new page discover and addded new slider type component

---
+++ pages/products/discover.vue
@@ -0,0 +1,15 @@
+<template>
+	<ModernCustomSlider
+		:headlineBlack="false"
+		:backgroundImage="bgImage11"
+		:isBackgroundImage="true"
+		:smallBigTitle="{ small: 'discover', big: 'What’s New' }"
+		:description="'The latest campaigns, collections and news from the World of Ralph Lauren'"
+	/>
+</template>
+
+<script setup>
+import bgImage11 from "~/assets/images/bg-group11.jpg";
+</script>
+
+<style scoped lang="scss"></style>
